8bf543cdc89aad7f151d6b83fea4c5aca2a3a771
Chris B <cbosoft@protonmail.com>
2023-08-01 21:30:06 +0100

    Note instructions as integers, not enum

--- gbemu/src/instructions.rs
+++ gbemu/src/instructions.rs
@@ -1,24 +1,21 @@
-use crate::registers::{TargetRegister8, TargetRegister16};
-
 // https://meganesu.github.io/generate-gb-opcodes/
-#[allow(non_camel_case_types)]
-#[derive(Clone, Debug)]
-pub enum Instruction {
+#[allow(non_upper_case_globals)]
+pub mod Instructions {
     /*
         0x00 NOP
         B1 C1
         Flags - - - -
         No operation.
     */
-    NoOperation,
+    pub const NO_OP: u8 = 0x00;
 
     /*
-        0x01 LD BC
+        0x01 LD BC, d16
         B3 C3
         Flags - - - -
         Load immediate two bytes into reg BC
     */
-    LoadIntoBCFromMemory(u16),
+    pub const LD_BC_d16: u8 = 0x01;
 
     /*
         0x02 LD (BC), A
@@ -26,7 +23,7 @@ pub enum Instruction {
         Flags - - - -
         Load contents of A into mem specified by BC
     */
-    LoadContentsOfAIntoMemoryAtBC,
+    pub const LD_aBC_A: u8 = 0x02;
 
     /*
         0x03 INC BC
@@ -34,7 +31,7 @@ pub enum Instruction {
         Flags - - - -
         Increment BC by 1
     */
-    IncrementBC,
+    pub const INC_BC: u8 = 0x03;
 
     /*
         0x04 INC B
@@ -42,7 +39,7 @@ pub enum Instruction {
         Flags Z 0 H -
         Increment B by 1
     */
-    IncrementB,
+    pub const INC_B: u8 = 0x04;
 
     /*
         0x05 DEC B
@@ -50,7 +47,7 @@ pub enum Instruction {
         Flags Z 1 H -
         Decrement B by 1
     */
-    DecrementB,
+    pub const DEC_B: u8 = 0x05;
 
     /*
         0x06 LD B, d8
@@ -58,7 +55,7 @@ pub enum Instruction {
         Flags - - - -
         Load immediate next byte into B
     */
-    LoadIntoB(u8),
+    pub const LD_B_d8: u8 = 0x06;
 
     /*
         0x07 RLCA
@@ -66,15 +63,15 @@ pub enum Instruction {
         Flags 0 0 0 A7
         rotate A left 1 (left-shift 1 and store pos 8 back in pos 0 as well as carry flag)
     */
-    RotateALeftCopyIntoCarry,
+    pub const RLCA: u8 = 0x07;
 
     /*
-        0x08 LD (a16, SP)
+        0x08 LD (a16), SP
         B3 C5
         Flags - - - -
         Store stack pointer value at address specified by next immediate word
     */
-    LoadStackPointerIntoMemory(u16),
+    pub const LD_a16_SP: u8 = 0x08;
 
     /*
         0x09 ADD HL, BC
@@ -82,7 +79,7 @@ pub enum Instruction {
         Flags - 0 H CY
         Add BC to HL, store in HL
     */
-    AddBCToHL,
+    pub const ADD_HL_CB: u8 = 0x09;
 
     /*
         0x0A LD A, (BC)
@@ -90,7 +87,7 @@ pub enum Instruction {
         Flags - - - -
         Load 8bits of memory at position specified by BC into A
     */
-    LoadIntoAFromMemoryAtBC,
+    pub const LD_A_aBC: u8 = 0x0A;
 
     /*
         0x0B DEC BC
@@ -98,21 +95,21 @@ pub enum Instruction {
         Flags - - - -
         decrement BC
     */
-    DecrementBC,
+    pub const DEC_BC: u8 = 0x0B;
 
     /*
         0x0C INC C
         B1 C1
         Flags Z 0 H -
     */
+    pub const INC_C: u8 = 0x0C;
     
-    IncrementC,
     /*
         0x0D DEC C
         B1 C1
         Flags Z 1 H 0
     */
-    DecrementC,
+    pub const DEC_C: u8 = 0x0D;
 
     /*
         0x0E LD C, d8
@@ -120,7 +117,7 @@ pub enum Instruction {
         Flags - - - -
         Load immediate next byte into C
     */
-    LoadIntoC(u8),
+    pub const LD_C_d8: u8 = 0x0E;
 
     /*
         0x0F RRCA
@@ -128,7 +125,7 @@ pub enum Instruction {
         Flags 0 0 0 A0
         Rotate A right, store A0 in A7 and carry
     */
-    RotateARightCopyIntoCarry,
+    pub const RRCA: u8 = 0x0F;
 
     // TODO ...
 
@@ -138,7 +135,7 @@ pub enum Instruction {
         Flags Z 0 H CY
         Add A and B, store result in A
     */
-    AddBToA,
+    pub const ADD_A_B: u8 = 0x80;
 
     /*
         0x81 ADD A, C
@@ -146,41 +143,16 @@ pub enum Instruction {
         Flags Z 0 H CY
         Add A and B, store result in A
     */
-    AddCToA,
+    pub const ADD_A_C: u8 = 0x81;
 
     // TODO ...
 
     /*
-        0F8 LD HL, SP+s8
+        0xF8 LD HL, SP+s8
         B2 C3
         Flags 0 0 H C
         Add signed integer 8 to stack pointer, store result in hl
     */
-    AddToSPStoreInHL(i8),
-}
+    pub const LD_HL_SP_ADD_s8: u8 = 0xF8;
 
-impl Instruction {
-    pub fn decode_bytes(bytes: Vec<u8>) -> Vec<Instruction> {
-        let mut i = 0usize;
-        let mut instructions = Vec::new();
-        while i < bytes.len() {
-            let byte = bytes[i];
-            let (inst, adv) = match byte {
-                0x00 => (Instruction::NoOperation, 1),
-                // ...                                             /* I think I may have the endian-ness wrong here */
-                0x08 => (Instruction::LoadStackPointerIntoMemory(((bytes[i+1] as u16) << 8) + (bytes[i+2] as u16)), 3),
-                // ...
-                0x0E => (Instruction::LoadIntoC(bytes[i+1]), 2),
-                // ...
-                0x80 => (Instruction::AddBToA, 1),
-                0x81 => (Instruction::AddCToA, 1),
-                // ...
-                0xF8 => (Instruction::AddToSPStoreInHL(bytes[i + 1] as i8), 2),
-                _ => { todo!(); }
-            };
-            instructions.push(inst);
-            i += adv;
-        }
-        instructions
-    }
 }
