0d6061fd3629cfdb106303d11118da6603969f52
Chris B <cbosoft@protonmail.com>
2023-08-01 22:47:01 +0100

    Sketch out implementation for most instructions up to op 0x20

--- gbemu/src/cpu.rs
+++ gbemu/src/cpu.rs
@@ -47,19 +47,58 @@ impl LR35902 {
     fn execute(&mut self, instruction: u8) {
         console_log(format!("Executing {instruction:?}").as_str());
         let cycles_passed = match instruction {
+
             instructions::NO_OP => 1,
-            // ...
-            // Instruction::LoadStackPointerIntoMemory(v) => { todo!(); },
-            // ...
-            instructions::LD_C_d8 => {
-                let arg = self.next_byte();
-                self.load_immediate8(TargetRegister8::C, arg)
-            },
-            // ...
-            instructions::ADD_A_B => self.add(TargetRegister8::B),
-            instructions::ADD_A_C => self.add(TargetRegister8::C),
-            // ...
-            _ => 1,
+            instructions::STOP => todo!(),
+
+            instructions::JR_s8 => self.jump_relative(),
+            instructions::JR_Z_s8 => self.conditional_jump_relative(Flag::Zero),
+            instructions::JR_C_s8 => self.conditional_jump_relative(Flag::Carry),
+
+            instructions::INC_B => self.increment8(Register8::B),
+            instructions::INC_C => self.increment8(Register8::C),
+            instructions::INC_D => self.increment8(Register8::D),
+            instructions::INC_E => self.increment8(Register8::E),
+
+            instructions::DEC_B => todo!(),
+            instructions::DEC_C => todo!(),
+            instructions::DEC_D => todo!(),
+            instructions::DEC_E => todo!(),
+
+            instructions::INC_BC => self.increment16(Register16::BC),
+            instructions::INC_DE => self.increment16(Register16::DE),
+
+            instructions::DEC_BC => todo!(),
+            instructions::DEC_DE => todo!(),
+
+            instructions::LD_B_d8 => self.load_byte_to_reg(Register8::B),
+            instructions::LD_C_d8 => self.load_byte_to_reg(Register8::C),
+            instructions::LD_D_d8 => self.load_byte_to_reg(Register8::D),
+            instructions::LD_E_d8 => self.load_byte_to_reg(Register8::E),
+
+            instructions::LD_BC_d16 => self.load_word_to_reg(Register16::BC),
+            instructions::LD_DE_d16 => self.load_word_to_reg(Register16::DE),
+
+            instructions::LD_A_aBC => self.load_mem_at_reg_to_reg(Register8::A, Register16::BC),
+            instructions::LD_A_aDE => self.load_mem_at_reg_to_reg(Register8::A, Register16::DE),
+
+            instructions::LD_aBC_A => self.load_reg_to_mem_at_reg(Register16::BC, Register8::A),
+            instructions::LD_aDE_A => self.load_reg_to_mem_at_reg(Register16::DE, Register8::A),
+
+            instructions::LD_a16_SP => todo!(),
+
+            instructions::RLCA => todo!(),
+            instructions::RRCA => todo!(),
+
+            instructions::RLA => todo!(),
+            instructions::RRA => todo!(),
+
+            instructions::ADD_A_B => self.add(Register8::B),
+            instructions::ADD_A_C => self.add(Register8::C),
+            instructions::ADD_HL_BC => self.add16(Register16::HL, Register16::BC),
+            instructions::ADD_HL_DE => self.add16(Register16::HL, Register16::DE),
+
+            0x20..=0xFF => todo!(),
         };
 
         self.cycle += cycles_passed;
@@ -72,6 +111,7 @@ impl LR35902 {
         }
     }
 
+    #[allow(unused)]
     fn run_n(&mut self, v: u16) {
         console_log(format!("Running instructions {} to {} of program", self.registers.pc, self.registers.pc.wrapping_add(v)).as_str());
         for _ in 0..v {
@@ -82,14 +122,69 @@ impl LR35902 {
     
     // instruction implementations...
 
-    fn load_immediate8(&mut self, target: TargetRegister8, value: u8) -> u64 {
-        self.registers.set8(target, value);
+    fn load_word_to_reg(&mut self, reg: Register16) -> u64 {
+        let value = self.next_word();
+        self.registers.set16(reg, value);
+        3
+    }
+
+    fn load_byte_to_reg(&mut self, reg: Register8) -> u64 {
+        let value = self.next_byte();
+        self.registers.set8(reg, value);
         2
     }
 
-    fn add(&mut self, target: TargetRegister8) -> u64 {
+    fn load_reg_to_mem_at_reg(&mut self, dest: Register16, src: Register8) -> u64 {
+        let addr = self.registers.get16(dest);
+        let value = self.registers.get8(src);
+        self.memory[addr] = value;
+        2
+    }
+
+    fn load_mem_at_reg_to_reg(&mut self, dest: Register8, src: Register16) -> u64 {
+        let addr = self.registers.get16(src);
+        let value = self.memory[addr];
+        self.registers.set8(dest, value);
+        2
+    }
+
+    fn increment8(&mut self, reg: Register8) -> u64 {
+        let v = self.registers.get8(reg);
+        let (v, carry) = v.overflowing_add(1);
+        self.registers.set8(reg, v);
+
+        let half_carry: bool = (v & 0xF) + (1 & 0xF) > 0xF;
+
+        self.registers.set_flags(
+            v == 0,
+            false,
+            half_carry,
+            carry,
+        );
+
+        1
+    }
+
+    fn increment16(&mut self, reg: Register16) -> u64 {
+        let v = self.registers.get16(reg);
+        let (v, carry) = v.overflowing_add(1);
+        self.registers.set16(reg, v);
+
+        let half_carry: bool = (v & 0xF) + (1 & 0xF) > 0xF;
+
+        // self.registers.set_flags(
+        //     v == 0,
+        //     false,
+        //     half_carry,
+        //     carry,
+        // );
+
+        1
+    }
+
+    fn add(&mut self, reg: Register8) -> u64 {
         let a = self.registers.af.0;
-        let v = self.registers.get8(target);
+        let v = self.registers.get8(reg);
         let (a, carry) = a.overflowing_add(v);
         self.registers.af.0 = a;
 
@@ -103,6 +198,45 @@ impl LR35902 {
         );
 
         1
+    }
+
+    fn add16(&mut self, left: Register16, right: Register16) -> u64 {
+        let a = self.registers.get16(left);
+        let b = self.registers.get16(right);
+        let (a, carry) = a.overflowing_add(b);
+        self.registers.set16(left, a);
+
+        let half_carry: bool = (a & 0xF) + (b & 0xF) > 0xF;
+
+        self.registers.set_flags(
+            a == 0,
+            false,
+            half_carry,
+            carry,
+        );
+
+        2
+    }
+
+    fn jump_relative(&mut self) -> u64 {
+        let s8 = self.next_byte() as i32;
+        let new_pc = (self.registers.pc as i32) + s8;
+        self.registers.pc = new_pc as u16;
+        3
+    }
+
+    fn conditional_jump_relative(&mut self, flag: Flag) -> u64 {
+        let s8 = self.next_byte() as i32;
+
+        if self.registers.get_flag(flag) {
+            let new_pc = (self.registers.pc as i32) + s8;
+            self.registers.pc = new_pc as u16;
+            3
+        }
+        else {
+            2
+        }
+
     }
 }
 
@@ -123,7 +257,7 @@ mod tests {
         cpu.run_n(2);
         assert_eq!(cpu.registers.af.0, 0x01);
         cpu.run_n(2);
-        assert_eq!(cpu.registers.get8(TargetRegister8::C), 0x12);
+        assert_eq!(cpu.registers.get8(Register8::C), 0x12);
         assert_eq!(cpu.registers.af.0, 0x13);
     }
 }
--- gbemu/src/instructions.rs
+++ gbemu/src/instructions.rs
@@ -3,7 +3,7 @@
 // https://meganesu.github.io/generate-gb-opcodes/
 
     
-//0x0*
+// 0x0*
 pub const NO_OP: u8 = 0x00;
 pub const LD_BC_d16: u8 = 0x01;
 pub const LD_aBC_A: u8 = 0x02;
@@ -13,13 +13,36 @@ pub const DEC_B: u8 = 0x05;
 pub const LD_B_d8: u8 = 0x06;
 pub const RLCA: u8 = 0x07;
 pub const LD_a16_SP: u8 = 0x08;
-pub const ADD_HL_CB: u8 = 0x09;
+pub const ADD_HL_BC: u8 = 0x09;
 pub const LD_A_aBC: u8 = 0x0A;
 pub const DEC_BC: u8 = 0x0B;
 pub const INC_C: u8 = 0x0C;
 pub const DEC_C: u8 = 0x0D;
 pub const LD_C_d8: u8 = 0x0E;
 pub const RRCA: u8 = 0x0F;
+
+// 0x1*
+pub const STOP: u8 = 0x10;
+pub const LD_DE_d16: u8 = 0x11;
+pub const LD_aDE_A: u8 = 0x12;
+pub const INC_DE: u8 = 0x13;
+pub const INC_D: u8 = 0x14;
+pub const DEC_D: u8 = 0x15;
+pub const LD_D_d8: u8 = 0x16;
+pub const RLA: u8 = 0x17;
+pub const JR_s8: u8 = 0x18;
+pub const ADD_HL_DE: u8 = 0x19;
+pub const LD_A_aDE: u8 = 0x1A;
+pub const DEC_DE: u8 = 0x1B;
+pub const INC_E: u8 = 0x1C;
+pub const DEC_E: u8 = 0x1D;
+pub const LD_E_d8: u8 = 0x1E;
+pub const RRA: u8 = 0x1F;
+
+// TODO ...
+
+pub const JR_Z_s8: u8 = 0x28;
+pub const JR_C_s8: u8 = 0x38;
 
 // TODO ...
 
